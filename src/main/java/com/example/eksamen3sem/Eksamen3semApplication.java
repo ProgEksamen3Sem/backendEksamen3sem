c8e2fabf8860bd2c1c2f3d7f5413be3a09f152b7
Yasi1095 <yasi1095@stud.kea.dk>
2024-01-09 16:03:43 +0100

    oprette projekt

---
+++ src/main/java/com/example/eksamen3sem/Eksamen3semApplication.java
@@ -0,0 +1,13 @@
+package com.example.eksamen3sem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eksamen3semApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Eksamen3semApplication.class, args);
+    }
+
+}
